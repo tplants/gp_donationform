3e1ada8ce4addff2c3bec48904592ad3689c70bb
tplants <todd@annelewisllc.com>
2015-11-18 00:23:46 -0500

    - Removed additional revenue events from conversion code. Not supported in free Optimizely plan. - Moved all experiment code to treatment.js. - Hide HTML tag in treatment to prevent FOUC.

--- treatment.js
+++ treatment.js
@@ -1,8 +1,27 @@
+$('html').hide();
+
+window.alsOptimizely = {};
+
+alsOptimizely.registeredVariations = [
+  {"varA": 3981, "varB": 3982}
+];
+
+alsOptimizely.activeVariations = {};
+
+for (var variationsIndex = 0; variationsIndex < alsOptimizely.registeredVariations.length; variationsIndex++ ) {
+  if(window.location.href.indexOf('df_id=' + alsOptimizely.registeredVariations[variationsIndex].varA) != -1) {
+    alsOptimizely.activeVariations.varA = alsOptimizely.registeredVariations[variationsIndex].varA;
+    alsOptimizely.activeVariations.varB = alsOptimizely.registeredVariations[variationsIndex].varB;
+    break;
+  }
+}
+
 if(alsOptimizely.activeVariations.varB) {
-  // Note: does not preserve URL parameters sent to original page.
-  window.location.replace("http://us.greenpeace.org/site/Donation2?df_id=" +
+  window.location.replace("http://us.greenpeace.org/site/Donation2?df_id=" +   // Note: does not preserve URL parameters sent to original page.
     alsOptimizely.activeVariations.varB +
     "&" +
     alsOptimizely.activeVariations.varB+
     ".donation=form1");
+} else {
+  $('html').show();
 }
